f7731672d2b51de205c3de367f77b8e41af29eb6
anup <sableanup999@gmail.com>
2019-11-26 02:01:01 +0630

    client carousal has been added

--- js/custom.js
+++ js/custom.js
@@ -126,7 +126,19 @@ $(function(){
 });
 
 
-
+//CLients
+$(function() {
+    $("#client-list").owlCarousel({
+        items: 6,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
 
 
 
