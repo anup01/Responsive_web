d9525215a5a94f244ec03bdd41d8984544f2f054
anup <sableanup999@gmail.com>
2019-11-25 16:13:49 +0630

    testimonial section added

--- js/custom.js
+++ js/custom.js
@@ -87,6 +87,38 @@ $(function() {
     });
 });
 
+// testimonial
+// Owl carousal code
+$(function() {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+        // responsive: {
+        //     // breakpoint from 0 up
+        //     0: {
+        //         items: 1
+        //     },
+        //     // breakpoint from 480 up
+        //     480: {
+        //         items: 2
+        //     }
+        // }
+    });
+});
+
+
+
+
+
+
+
+
 
 
 
